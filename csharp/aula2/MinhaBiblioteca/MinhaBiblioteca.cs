ad0e6d317848b049af9adfa2973da9152535ca69
andersqz <anderson.carvalho@ufn.edu.br>
2026-03-15 06:07:20 -0300

    funcao 1 de python refatorado para C# - aula2

---
+++ csharp/aula2/MinhaBiblioteca/MinhaBiblioteca.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class MinhaBiblioteca
+{
+    static void PopularAleatorio(List<int> lista, int quantidade)
+    {
+        // instancia o gerador de numeros aleatorios e ao msm tempo define uma semente no tempo atual
+        // diferente de C++ que precisa definir a semente separado exemplo: '(srand(time(0)))'
+        Random random = new Random();
+
+        for (int i = 0; i < quantidade; i++)
+        {
+            // equivalente ao push_back() do C++ e ao .append() do python
+            lista.Add(random.Next(0, 101));
+        }
+    }
+}
